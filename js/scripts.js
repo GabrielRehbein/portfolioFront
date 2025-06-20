dfc922d570120f81d6e523c6a45987f30e813627
gabrielrehbein <gabrielrehbei@gmail.com>
2025-06-20 12:12:13 -0300

    feat: base carousel of projects

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,37 @@ sendEmailForm.addEventListener("submit", (e) => {
     }
 });
 
+
+const slide = document.querySelector('#slide');
+const imgs = document.querySelectorAll('.project-img');
+const containerProject = document.querySelector('.container-project');
+
+let idImageSlide = 0;
+
+//TODO adicionar os links do projeto no "footer" da imagem do projeto(linkedin e github)
+containerProject.addEventListener('click', (e) => {
+    const rightArrowClassName = 'fa-arrow-right';
+    const leftArrowClassName = 'fa-arrow-left';
+
+    if (e.target.classList.contains(rightArrowClassName)){
+        imgs[idImageSlide].classList.add('hide');
+        idImageSlide++;
+        
+        if (idImageSlide > imgs.length - 1){
+            idImageSlide = 0;
+        }
+
+        imgs[idImageSlide].classList.remove('hide');
+    }
+    if (e.target.classList.contains(leftArrowClassName)){
+        imgs[idImageSlide].classList.add('hide');
+        idImageSlide--;
+
+        if (idImageSlide < 0){
+            idImageSlide = imgs.length - 1;
+        }
+
+        imgs[idImageSlide].classList.remove('hide');
+    }
+})
+
