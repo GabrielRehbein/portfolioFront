0845ee685611853a1fd336a2d0519457e8d07690
gabrielrehbein <gabrielrehbei@gmail.com>
2025-06-20 20:24:21 -0300

    feat: add dynamic links in my projects[footer](linkedin and github) section

--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,7 @@ sendEmailForm.addEventListener("submit", (e) => {
 const slide = document.querySelector('#slide');
 const imgs = document.querySelectorAll('.project-img');
 const containerProject = document.querySelector('.container-project');
-
+const projectInfo = document.querySelectorAll('.project-info');
 let idImageSlide = 0;
 
 //TODO adicionar os links do projeto no "footer" da imagem do projeto(linkedin e github)
@@ -46,6 +46,7 @@ containerProject.addEventListener('click', (e) => {
 
     if (e.target.classList.contains(rightArrowClassName)){
         imgs[idImageSlide].classList.add('hide');
+        projectInfo[idImageSlide].classList.add('hide');
         idImageSlide++;
         
         if (idImageSlide > imgs.length - 1){
@@ -53,16 +54,19 @@ containerProject.addEventListener('click', (e) => {
         }
 
         imgs[idImageSlide].classList.remove('hide');
+        projectInfo[idImageSlide].classList.remove('hide');
     }
     if (e.target.classList.contains(leftArrowClassName)){
         imgs[idImageSlide].classList.add('hide');
+        projectInfo[idImageSlide].classList.add('hide');
         idImageSlide--;
 
         if (idImageSlide < 0){
             idImageSlide = imgs.length - 1;
         }
-
+         
         imgs[idImageSlide].classList.remove('hide');
+        projectInfo[idImageSlide].classList.remove('hide');   
     }
 })
 
