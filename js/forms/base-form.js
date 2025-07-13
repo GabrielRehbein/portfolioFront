d6f76fa8d18ab6a89acbd9245736fc5bad23c0d2
GabrielRehbein <gabrielrehbei@gmail.com>
2025-07-13 18:26:08 -0300

    feat: create getInputsData in BaseForm and add preventDefault in createForm

--- js/forms/base-form.js
+++ js/forms/base-form.js
@@ -1,3 +1,12 @@
 export default class Form {
-    
+    constructor(form){
+        this.form = form;
+    }
+
+    getInputsData(){
+        const formData = new FormData(this.form);
+        const data = Object.fromEntries(formData.entries());
+        console.log(data);
+    }
+
 }
--- js/forms/project-form.js
+++ js/forms/project-form.js
@@ -4,20 +4,49 @@ import BaseForm from "./base-form.js"
 const createProjectScreen = document.querySelector("#create-project-screen")
 const btnCreateProject = document.querySelector("#btn-create-project")
 const headerAdm = document.querySelector("#header-adm")
-
+const createForm = document.querySelector("#create-project-form")
 const admScreen = document.querySelector("#main-adm")
+const btnRegisterProject = document.querySelector("#btn-register-project-final")
 
 export default class CreateProjectForm extends BaseForm {
 
     constructor(){
-        super();
+        super(createForm);
         this.screenForm = createProjectScreen;
+        this.addPreventDefault();
+        this.createProject();
     }
+
     openFormScreen(){
         btnCreateProject.addEventListener('click', (e) => {
         e.preventDefault();
-        changeScreen(createProjectForm, admScreen);
-        changeScreen(createProjectForm, headerAdm);
-    })
+        changeScreen(this.screenForm, admScreen);
+        changeScreen(this.screenForm, headerAdm);
+        })
+    }
 
-}}
+    addPreventDefault() {
+        if (this.form) {
+            this.form.addEventListener('submit', (e) => {
+                e.preventDefault();
+            });
+        } else {
+            console.warn("createForm não está definido.");
+        }
+    }
+
+    
+
+    createProject(){
+        btnRegisterProject.addEventListener('click', (e) => {
+            e.preventDefault();
+            // Pegar e (outra funcao validade os dados) e criar um dicionario
+            const data = this.getInputsData();
+            // Chamar API EX: ProductService.create(dados)
+            console.log('projeto criado')
+            changeScreen(admScreen, this.screenForm);
+            changeScreen(headerAdm, this.screenForm);
+        })
+    }
+
+}
